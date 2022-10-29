fe342c6aa3ab7fcdae83eeb51c2b33da2a5d29d4
Christof <bauer.christof@gmail.com>
2022-10-29 18:21:43 -0500

    Name conflict

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import { addDataLayer } from '../lib/addDataLayer'
 import { data } from '../lib/data'
 import Head from 'next/head'
 import 'mapbox-gl/dist/mapbox-gl.css'
-
-import Overlay from '../components/overlay'
+import Overlay from '../components/Overlay'
 const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
 
 const MapComponent = () => {
   const [Map, setMap] = useState()
   const [pageIsMounted, setPageIsMounted] = useState(false)
+  const [popupChanged, setPopupChanged] = useState(false)
   const [overlayOpen, setOverlayOpen] = useState(true)
 
   mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
@@ -26,10 +26,10 @@ const MapComponent = () => {
       // style: 'mapbox://styles/mapbox/streets-v11',
       // style: 'mapbox://styles/mapbox/navigation-night-v1',
       center: [84, 28.5],
-      zoom: 9,
-      // pitch: 45,
+      zoom: 9.5,
+      pitch: 45,
     })
-    initializeMap(mapboxgl, map)
+    initializeMap(map, setPopupChanged)
     setMap(map)
   }, [])
 
@@ -37,9 +37,26 @@ const MapComponent = () => {
     if (pageIsMounted && data) {
       Map.on('load', () => {
         addDataLayer(Map, data)
+        Map.addControl(new mapboxgl.NavigationControl());
       })
     }
   }, [pageIsMounted, setMap, Map])
+
+
+  useEffect(() => {
+    console.log("effect.......");
+    console.log(popupChanged);
+
+    const btn = document.getElementsByClassName("next")[0]
+    console.log(btn);
+    if (btn) {
+      btn.addEventListener("click", () => {
+        const nextId = (parseInt(btn.getAttribute('data-id')) + 1).toString()
+        console.log("herehrhehrh");
+        openNextPopup(nextId, Map)
+      })
+    }
+  }, [popupChanged])
 
   return (
     <>
